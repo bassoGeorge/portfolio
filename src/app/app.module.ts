1735543f11a057775f58cdd2bef80315e8efea05
Anish George <anish.george@ignitesol.com>
2017-11-07 22:26:06 +0530

    Got the basic routing system in place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomePage } from './shared/pages/home/home.page';
+import { AboutMePage } from './shared/pages/about-me/about-me.page';
+
+const appRoutes: Routes = [
+    { path: '', component: HomePage },
+    { path: 'about-me', component: AboutMePage },
+    { path: '**', redirectTo: '' }
+]
 
 // Some auxilary style imports
 import '../assets/css/normalize.css';
 import '../assets/css/site.styl';
 
 @NgModule({
-    imports: [ BrowserModule ],
-    declarations: [ AppComponent ],
+    imports: [
+        RouterModule.forRoot(
+            appRoutes,
+            { enableTracing: true } // only for debugging purposes
+        ),
+        BrowserModule
+    ],
+    declarations: [
+        AppComponent,
+        HomePage,
+        AboutMePage
+    ],
     bootstrap: [ AppComponent ]
 })
 export class AppModule { }
