d9c0f35f9b80c399ff46e1ae8d3fb2364a17944c
Anish George <anish.george@ignitesol.com>
2017-12-10 10:21:37 +0530

    [WIP] Get svg inline for navigation

--- src/app/core/components/navigation/navigation.component.ts
+++ src/app/core/components/navigation/navigation.component.ts
@@ -3,11 +3,12 @@
 ///////////////////////////////////////////////////////////////////////////////
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 interface PageInfo {
     id: string;
     name: string;
-    img: string;
+    img: SafeHtml;
     imgActive: string;
     target: string;
 }
@@ -18,36 +19,39 @@ interface PageInfo {
     styleUrls: ['./navigation.component.styl']
 })
 export class NavigationComponent {
-    constructor(private router: Router) {}
+    constructor(
+        private router: Router,
+        private ds: DomSanitizer
+    ) {}
 
     public pages: PageInfo[] = [{
         id: 'home',
         name: "Home",
-        img: require('./assets/home_grey.svg'),
+        img: this.ds.bypassSecurityTrustHtml(require('!svg-inline-loader!./assets/home_grey.svg')),
         imgActive: require('./assets/home_green.svg'),
         target: "/home"
     }, {
         id: 'about',
         name: "About Me",
-        img: require('./assets/about_grey.svg'),
+        img: this.ds.bypassSecurityTrustHtml(require('!svg-inline-loader!./assets/about_grey.svg')),
         imgActive: require('./assets/about_green.svg'),
         target: "/about-me"
     }, {
         id: 'my-work',
         name: "My Work",
-        img: require('./assets/work_grey.svg'),
+        img: this.ds.bypassSecurityTrustHtml(require('!svg-inline-loader!./assets/work_grey.svg')),
         imgActive: require('./assets/work_green.svg'),
         target: "/my-work"
     }, {
         id: 'skills',
         name: "Skills",
-        img: require('./assets/skills_grey.svg'),
+        img: this.ds.bypassSecurityTrustHtml(require('!svg-inline-loader!./assets/skills_grey.svg')),
         imgActive: require('./assets/skills_green.svg'),
         target: "/skills"
     }, {
         id: 'contact-me',
         name: "Contact Me",
-        img: require('./assets/contact_grey.svg'),
+        img: this.ds.bypassSecurityTrustHtml(require('!svg-inline-loader!./assets/contact_grey.svg')),
         imgActive: require('./assets/contact_green.svg'),
         target: "/contact-me"
     }];
