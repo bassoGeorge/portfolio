dd3e281c318f0c59ac7213a119b73e35066f1c96
Anish George <anish.george@ignitesol.com>
2018-02-01 10:59:25 +0530

    Move to a better docker configuration

--- src/app/core/interceptors/data-api.interceptor.ts
+++ src/app/core/interceptors/data-api.interceptor.ts
@@ -18,8 +18,9 @@ export class DataApiInterceptor implements HttpInterceptor {
         // If it is not an absolute url, we route it to directus
         // TODO: get the directus base from configuration
         if (request.url.indexOf('http') !== 0) {
+            let apiRoot = window.location.protocol + "//" + window.location.hostname + ":8888";
             return next.handle(request.clone({
-                url: 'http://localhost:8888/api/1.1' + request.url
+                url: apiRoot + "/api/1.1" + request.url
             }));
         } else return next.handle(request);
     }
